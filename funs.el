fceade24596d895792319c6eb1713d29a532d5f7
Mikołaj Baranowski <mikolajb@gmail.com>
2010-01-04 14:45:47 +0100

    small changes

---
+++ funs.el
@@ -0,0 +1,11 @@
+(defun bf-pretty-print-xml-region (begin end)
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+      (backward-char) (insert "\n"))
+    (indent-region begin end))
+  (message "Ah, much better!"))
+
+(provide 'funs)
