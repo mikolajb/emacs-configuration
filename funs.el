28d10f82b0a667edcb657267c58dd8730ff58b24
Mikołaj Baranowski <mikolajb@gmail.com>
2010-04-07 14:44:49 +0200

    fun for swapping windows, color theme changed for zenburn

--- funs.el
+++ funs.el
@@ -1,4 +1,4 @@
-(defun bf-pretty-print-xml-region (begin end)
+(defun pretty-print-xml-region (begin end)
   (interactive "r")
   (save-excursion
     (nxml-mode)
@@ -7,5 +7,25 @@
       (backward-char) (insert "\n"))
     (indent-region begin end))
   (message "Ah, much better!"))
+
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((/= (count-windows) 2)
+         (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1))))
+  (other-window 1))
+
+;; (global-set-key (kbd "C-c s") 'swap-windows)
 
 (provide 'funs)
--- theme.el
+++ theme.el
@@ -1,6 +1,8 @@
 (require 'color-theme)
 (color-theme-initialize)
-(load "~/.emacs.d/plugins/color-theme-ir-black/color-theme-ir-black")
-(color-theme-ir-black)
+;; (load "~/.emacs.d/plugins/color-theme-ir-black/color-theme-ir-black")
+;; (color-theme-ir-black)
+(load "~/.emacs.d/plugins/color-theme-zenburn/zenburn")
+(color-theme-zenburn)
 
 (provide 'theme)
