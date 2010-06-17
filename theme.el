95928d1fe7a64cb905e9ba38333ca4b69209ae77
Mikołaj Baranowski <mikolajb@gmail.com>
2010-06-18 01:44:43 +0200

    unused themes removed

--- theme.el
+++ theme.el
@@ -1,26 +1,7 @@
 (require 'color-theme)
 (color-theme-initialize)
-;; git clone git://github.com/burke/color-theme-ir-black.git
-;; (load "~/.emacs.d/plugins/color-theme-ir-black/color-theme-ir-black")
-;; (color-theme-ir-black)
-;; (load "~/.emacs.d/plugins/color-theme-zenburn/zenburn")
-;; (color-theme-zenburn)
-;; git clone git://github.com/crafterm/twilight-emacs.git
-;; (load "~/.emacs.d/plugins/color-theme-twilight/color-theme-twilight")
-;; (color-theme-twilight)
-;; git clone git://jblevins.org/git/misc.git
-;; (load "~/.emacs.d/plugins/color-theme-jblevins.org/color-theme-subdued.el")
-;; (color-theme-subdued)
-;; (load "~/.emacs.d/plugins/color-theme-tango/color-theme-tango.el")
-;; (color-theme-tango)
-;; git clone git://github.com/wfarr/color-theme-tango-2.git
-;; (load "~/.emacs.d/plugins/color-theme-tango-2/color-theme-tango-2.el")
-;; (color-theme-tango-2)
-;; git clone git://github.com/dz/ir_black_emacs.git
+
 (load "~/.emacs.d/plugins/color-theme-irblack/color-theme-irblack")
 (color-theme-irblack)
-;; hg clone http://bitbucket.org/lloyda2/color-theme-molokai
-;; (load "~/.emacs.d/plugins/color-theme-molokai/color-theme-molokai")
-;; (color-theme-molokai)
 
 (provide 'theme)
