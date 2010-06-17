15462bb0a7aaadc905a09408f365676c241e5305
Mikołaj Baranowski <mikolajb@gmail.com>
2010-06-18 01:40:16 +0200

    new fun for renaming buffer and file at ones

--- funs.el
+++ funs.el
@@ -28,4 +28,22 @@
 
 ;; (global-set-key (kbd "C-c s") 'swap-windows)
 
+;; source: http://blog.tuxicity.se/elisp/emacs/2010/03/26/rename-file-and-buffer-in-emacs.html
+(defun rename-file-and-buffer ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (message "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file name new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)))))))
+;; (global-set-key (kbd "C-c r") 'rename-file-and-buffer)
+
 (provide 'funs)
