4baea8023bfab45137b4627920244944ea031019
Mikołaj Baranowski <mikolajb@gmail.com>
2010-08-23 13:37:07 +0200

    new function - duplicate line copied from emacs-fu blog

--- funs.el
+++ funs.el
@@ -46,4 +46,25 @@
                (set-buffer-modified-p nil)))))))
 ;; (global-set-key (kbd "C-c r") 'rename-file-and-buffer)
 
+(defun duplicate-line (&optional commentfirst)
+  "comment line at point; if COMMENTFIRST is non-nil, comment the original"
+  (interactive)
+  (beginning-of-line)
+  (push-mark)
+  (end-of-line)
+  (let ((str (buffer-substring (region-beginning) (region-end))))
+    (when commentfirst
+    (comment-region (region-beginning) (region-end)))
+    (insert-string
+      (concat (if (= 0 (forward-line 1)) "" "\n") str "\n"))
+    (forward-line -1)))
+
+;; or choose some better bindings....
+
+;; duplicate a line
+(global-set-key (kbd "C-c y") 'duplicate-line)
+
+;; duplicate a line and comment the first
+(global-set-key (kbd "C-c c") (lambda()(interactive)(duplicate-line t)))
+
 (provide 'funs)
