710bd284b6046162d9cc1ea52d3dda3d2b478e34
Mikołaj Baranowski <mikolajb@gmail.com>
2010-09-28 00:54:17 +0200

    cursor color depends on mode (read only, overwrite)

--- theme.el
+++ theme.el
@@ -7,4 +7,21 @@
 (load "~/.emacs.d/plugins/twilight-emacs/color-theme-twilight.el")
 (color-theme-twilight)
 
+;; Change cursor color according to mode
+(defvar hcz-set-cursor-color-color "")
+(defvar hcz-set-cursor-color-buffer "")
+(defun hcz-set-cursor-color-according-to-mode ()
+  "change cursor color according to some minor modes."
+  ;; set-cursor-color is somewhat costly, so we only call it when needed:
+  (let ((color
+	 (if buffer-read-only "white"
+	   (if overwrite-mode "red"
+	     "yellow green"))))
+    (unless (and
+	     (string= color hcz-set-cursor-color-color)
+	     (string= (buffer-name) hcz-set-cursor-color-buffer))
+      (set-cursor-color (setq hcz-set-cursor-color-color color))
+      (setq hcz-set-cursor-color-buffer (buffer-name)))))
+(add-hook 'post-command-hook 'hcz-set-cursor-color-according-to-mode)
+
 (provide 'theme)
