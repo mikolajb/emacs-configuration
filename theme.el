9333b6df4082077d6312d872d2f7e6338da49251
Mikołaj Baranowski <mikolajb@gmail.com>
2010-11-03 22:58:44 +0100

    irblack color theme is set, select-cursor hook modified

--- theme.el
+++ theme.el
@@ -1,27 +1,18 @@
 (require 'color-theme)
 (color-theme-initialize)
 
-;; (load "~/.emacs.d/plugins/color-theme-irblack/color-theme-irblack")
-;; (color-theme-irblack)s
+(load "~/.emacs.d/plugins/color-theme-irblack/color-theme-irblack")
+(color-theme-irblack)
 
-(load "~/.emacs.d/plugins/twilight-emacs/color-theme-twilight.el")
-(color-theme-twilight)
-
-;; Change cursor color according to mode
-(defvar hcz-set-cursor-color-color "")
-(defvar hcz-set-cursor-color-buffer "")
-(defun hcz-set-cursor-color-according-to-mode ()
-  "change cursor color according to some minor modes."
-  ;; set-cursor-color is somewhat costly, so we only call it when needed:
-  (let ((color
-	 (if buffer-read-only "white"
-	   (if overwrite-mode "red"
-	     "yellow green"))))
-    (unless (and
-	     (string= color hcz-set-cursor-color-color)
-	     (string= (buffer-name) hcz-set-cursor-color-buffer))
-      (set-cursor-color (setq hcz-set-cursor-color-color color))
-      (setq hcz-set-cursor-color-buffer (buffer-name)))))
-(add-hook 'post-command-hook 'hcz-set-cursor-color-according-to-mode)
+(defun select-cursor-color-according-to-mode ()
+  (blink-cursor-mode 0)
+  (cond
+   (buffer-read-only
+    (set-cursor-color "gold"))
+   (overwrite-mode
+    (set-cursor-color "magenta3"))
+   (t
+    (set-cursor-color "tomato2"))))
+(add-hook 'post-command-hook 'select-cursor-color-according-to-mode)
 
 (provide 'theme)
