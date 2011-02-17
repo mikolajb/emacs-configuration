73b57b2269d9a1f3c0127693e3e10476f691e2fb
Mikołaj Baranowski <mikolajb@gmail.com>
2011-02-18 00:58:33 +0100

    elpa package repositories added (requires elpa from emacs 24)

--- init.el
+++ init.el
@@ -3,5 +3,14 @@
 (require 'modes)
 (require 'plugins)
 (require 'funs)
+(require 'snippets)
+
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 
 (provide 'init)
