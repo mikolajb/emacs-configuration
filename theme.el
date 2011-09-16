9acb07d4a4244c845f184c165d644a694007539a
Mikołaj Baranowski <mikolajb@gmail.com>
2011-09-16 09:04:53 +0200

    color theme irblack changed to color theme solarized

--- theme.el
+++ theme.el
@@ -1,16 +1,17 @@
 (require 'color-theme)
 (color-theme-initialize)
 
-(load "~/.emacs.d/plugins/color-theme-irblack/color-theme-irblack")
-(color-theme-irblack)
+(add-to-list 'load-path "~/.emacs.d/plugins/color-theme-solarized")
+(require 'color-theme-solarized)
+(color-theme-solarized 'light)
 
 (defun select-cursor-color-according-to-mode ()
   (blink-cursor-mode 0)
   (cond
    (buffer-read-only
-    (set-cursor-color "gold"))
+    (set-cursor-color "blue"))
    (overwrite-mode
-    (set-cursor-color "magenta3"))
+    (set-cursor-color "orange"))
    (t
     (set-cursor-color "#D4213D"))))
 (add-hook 'post-command-hook 'select-cursor-color-according-to-mode)
