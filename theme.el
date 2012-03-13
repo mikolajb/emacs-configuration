d3673ed16ca3dab1d5ac35652031807153322389
Mikołaj Baranowski <mikolajb@gmail.com>
2012-03-13 19:14:56 +0100

    new default theme - tango dark
    better terminal colors

--- theme.el
+++ theme.el
@@ -1,19 +1,41 @@
-(require 'color-theme)
-(color-theme-initialize)
-
-(add-to-list 'load-path "~/.emacs.d/plugins/color-theme-solarized")
-(require 'color-theme-solarized)
-(color-theme-solarized 'light)
+(load-theme 'tango-dark)
 
 (defun select-cursor-color-according-to-mode ()
   (blink-cursor-mode 0)
   (cond
    (buffer-read-only
-    (set-cursor-color "blue"))
+    (set-cursor-color "DodgerBlue"))
    (overwrite-mode
     (set-cursor-color "orange"))
+   ((buffer-modified-p)
+    (set-cursor-color "#D4213D"))
    (t
-    (set-cursor-color "#D4213D"))))
+    (set-cursor-color "#E9E8E7"))))
 (add-hook 'post-command-hook 'select-cursor-color-according-to-mode)
+
+;;; terminal colors
+(setq ansi-color-for-comint-mode t)
+(setq ansi-color-for-comint-mode-on t)
+(setq ansi-color-names-vector
+      ["#000000"           ; black
+       "#CC0000"           ; red
+       "#4E9A06"           ; green
+       "#C4A000"           ; yellow
+       "#3465A4"           ; blue
+       "#75507B"           ; magenta
+       "#06989A"           ; cyan
+       "#D3D7CF"]          ; white
+      )
+(setq ansi-term-color-vector
+      [unspecified
+       "#555753"           ; black
+       "#EF2929"           ; red
+       "#8AE234"           ; green
+       "#FCE94F"           ; yellow
+       "#729FCF"           ; blue
+       "#AD7FA8"           ; magenta
+       "#34E2E2"           ; cyan
+       "#EEEEEC"]          ; white
+      )
 
 (provide 'theme)
