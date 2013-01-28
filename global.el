909b7f25437a18c3417b3b5f0a28aa4a339a9706
Mikołaj Baranowski <mikolajb@gmail.com>
2013-01-28 17:53:32 +0100

    desktop save mode turned off

--- global.el
+++ global.el
@@ -23,30 +23,6 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
-;;; Desktops
-(setq desktop-restore-eager 20
-      desktop-lazy-verbose nil)
-(desktop-save-mode 1)
-
-;;; desktop-override-stale-locks begins here
-(defun emacs-process-p (pid)
-  "If pid is the process ID of an emacs process, return t, else nil.
-Also returns nil if pid is nil."
-  (when pid
-    (let* ((cmdline-file (concat "/proc/" (int-to-string pid) "/cmdline")))
-      (when (file-exists-p cmdline-file)
-	(with-temp-buffer
-	  (insert-file-contents-literally cmdline-file)
-	  (goto-char (point-min))
-	  (search-forward "emacs" nil t)
-	  pid)))))
-
-(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
-  "Don't allow dead emacsen to own the desktop file."
-  (when (not (emacs-process-p ad-return-value))
-    (setq ad-return-value nil)))
-;;; desktop-override-stale-locks ends here
-
 ;;; auto revert modified files
 (global-auto-revert-mode t)
 
