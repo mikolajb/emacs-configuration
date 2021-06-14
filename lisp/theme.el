0df48580824fa04be0b041bab58a25f8d3fa660a
Mikołaj Baranowski <mikolajb@gmail.com>
2021-06-14 23:22:04 +0200

    Add nerd font symbols
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- lisp/theme.el
+++ lisp/theme.el
@@ -36,6 +36,7 @@
 (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
 (set-fontset-font t 'unicode "Noto Sans Symbols" nil 'prepend)
 (set-fontset-font t 'unicode "Noto Sans Display" nil 'prepend)
+(set-fontset-font t 'unicode "Nerd Font Symbols" nil 'prepend)
 
 (custom-set-faces
  '(variable-pitch ((t (:family "Museo")))))
